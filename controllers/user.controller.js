3d07467f35ab6cb6404100532cd4f69fa82d0d20
Sanju Raj Prasad <43962657+Blazexsam27@users.noreply.github.com>
2025-06-10 22:16:46 +0200

    Updated user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ const userService = require("../services/user.service");
 
 exports.getUser = async (req, res) => {
   try {
-    const user = await userService.getUser(req.user.id);
+    const user = await userService.getUser(req.params.id);
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error.message);
